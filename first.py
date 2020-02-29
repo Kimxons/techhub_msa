58d948642a7d25303e7422c0013222ae787499d8
meshKitonga <kitongameshack9@gmail.com>
2020-02-29 08:31:14 -0500

    Update first.py

--- first.py
+++ first.py
@@ -1,12 +1,11 @@
-# num = 3
-# if(num !=4):
-#     print("not equal")
-# else:
-#     print("you got it")
+num = 3
+if(num !=4):
+    print("not equal")
+else:
+    print("you got it")
  
-
-# list = [1, 2, 3, 4]
-# for i in range(0, len(list)):
-#     print(list[i])
+list = [1, 2, 3, 4]
+for i in range(0, len(list)):
+    print(list[i])
 
 
