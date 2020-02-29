938c74c65b511315b6cbdb7047dcd40ea1e9fd1a
meshKitonga <kitongameshack9@gmail.com>
2020-02-29 08:32:05 -0500

    Update first.py

--- first.py
+++ first.py
@@ -1,8 +1,8 @@
 num = 3
-if(num !=4):
-    print("not equal")
+if(num ==4):
+    print("correct")
 else:
-    print("you got it")
+    print("you got to try again dude!")
  
 list = [1, 2, 3, 4]
 for i in range(0, len(list)):
